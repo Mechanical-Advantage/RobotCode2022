182535132afa9db5689f7e53941dc5b4f2dd04a4
Cameron Earle <camearle20@me.com>
2022-01-10 19:17:09 -0500

    Add basic drivetrain class logic
    Co-authored-by: Aum-P <Aum-P@users.noreply.github.com>
    Co-authored-by: rohansun23 <rohansun23@users.noreply.github.com>
    Co-authored-by: AryanKulkarni <AryanKulkarni@users.noreply.github.com>

--- src/main/java/frc/robot/subsystems/drivetrain/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/drivetrain/DriveTrain.java
@@ -8,11 +8,16 @@ import java.util.function.Supplier;
 
 import org.littletonrobotics.junction.Logger;
 
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 import frc.robot.commands.SysIdCommand.DriveTrainSysIdData;
 import frc.robot.subsystems.drivetrain.DriveTrainIO.DriveTrainIOInputs;
 
 public class DriveTrain extends SubsystemBase {
+  private final double wheelRadiusM;
+  private final SimpleMotorFeedforward leftModel, rightModel;
 
   private final DriveTrainIO io;
   private final DriveTrainIOInputs inputs = new DriveTrainIOInputs();
@@ -23,6 +28,31 @@ public class DriveTrain extends SubsystemBase {
   /** Creates a new DriveTrain. */
   public DriveTrain(DriveTrainIO io) {
     this.io = io;
+
+    switch (Constants.getRobot()) {
+      // case ROBOT_2022C:
+      // break;
+      case ROBOT_2020:
+        wheelRadiusM = Units.inchesToMeters(3.0);
+        leftModel = new SimpleMotorFeedforward(0, 0, 0);
+        rightModel = new SimpleMotorFeedforward(0, 0, 0);
+        break;
+      // case ROBOT_2022P:
+      // break;
+      // case ROBOT_KITBOT:
+      // break;
+      // case ROBOT_ROMI:
+      // break;
+      // case ROBOT_SIMBOT:
+      // break;
+      default:
+        wheelRadiusM = Double.POSITIVE_INFINITY;
+        leftModel = new SimpleMotorFeedforward(0, 0, 0);
+        rightModel = new SimpleMotorFeedforward(0, 0, 0);
+
+        break;
+
+    }
   }
 
   /** Set boolean supplier for the override switches. */
@@ -42,28 +72,51 @@ public class DriveTrain extends SubsystemBase {
   /**
    * Drive at the specified voltage with no other processing. Only use with SysId.
    */
-  public void driveVoltage(double leftVolts, double rightVolts) {}
+  public void driveVoltage(double leftVolts, double rightVolts) {
+    io.setVoltage(leftVolts, rightVolts);
+  }
 
   /**
    * Drive at the specified percentage of max speed.
    */
-  public void drivePercent(double leftPercent, double rightPercent) {}
+  public void drivePercent(double leftPercent, double rightPercent) {
+    io.setVoltage(leftPercent * 12.0, rightPercent * 12.0);
+  }
 
   /**
    * Drive at the specified velocity.
    */
   public void driveVelocity(double leftVelocityMetersPerSec,
-      double rightVelocityMetersPerSec) {}
+      double rightVelocityMetersPerSec) {
+    double leftVelocityRadPerSec = leftVelocityMetersPerSec / wheelRadiusM;
+    double rightVelocityRadPerSec = rightVelocityMetersPerSec / wheelRadiusM;
+
+    double leftFFVolts = leftModel.calculate(leftVelocityRadPerSec);
+    double rightFFVolts = rightModel.calculate(rightVelocityRadPerSec);
+
+    if (openLoopOverride.get()) {
+      // Use open loop control
+      io.setVoltage(leftFFVolts, rightFFVolts);
+    } else {
+      io.setVelocity(leftVelocityRadPerSec,
+          rightVelocityRadPerSec / wheelRadiusM, leftFFVolts, rightFFVolts);
+    }
+  }
 
   /**
    * In open loop, goes to neutral. In closed loop, resets velocity setpoint.
    */
-  public void stop() {}
+  public void stop() {
+    driveVelocity(0, 0);
+  }
 
   /**
    * Returns a set of data for SysId
    */
   public DriveTrainSysIdData getSysIdData() {
-    return new DriveTrainSysIdData(0.0, 0.0, 0.0, 0.0, 0.0, 0.0);
+    return new DriveTrainSysIdData(inputs.leftPositionRad,
+        inputs.rightPositionRad, inputs.leftVelocityRadPerSec,
+        inputs.rightVelocityRadPerSec, inputs.gyroPositionRad,
+        inputs.gyroVelocityRadPerSec);
   }
 }
