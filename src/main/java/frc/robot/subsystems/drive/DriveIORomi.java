f3ebf3624fbbf1ff06030031b12905307e35ea48
David Ming <dming.2024@littletonps.org>
2022-01-20 20:25:44 -0500

    start working on romi implementation of drivetrain

---
+++ src/main/java/frc/robot/subsystems/drive/DriveIORomi.java
@@ -0,0 +1,70 @@
+package frc.robot.subsystems.drive;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
+import edu.wpi.first.wpilibj.romi.RomiGyro;
+import frc.robot.Constants;
+
+public class DriveIORomi implements DriveIO {
+
+  private boolean closedLoop = false;
+  private final RomiGyro gyro = new RomiGyro();
+  private final Encoder leftEncoder = new Encoder(4, 5);
+  private final Encoder rightEncoder = new Encoder(6, 7);
+  private final Spark leftMotor = new Spark(0);
+  private final Spark rightMotor = new Spark(1);
+  private PIDController leftPID = new PIDController(0.0, 0.0, 0.0);
+  private PIDController rightPID = new PIDController(0.0, 0.0, 0.0);
+  private double leftFFVolts = 0.0;
+  private double rightFFVolts = 0.0;
+
+
+  public DriveIORomi() {}
+
+  @Override
+  public void updateInputs(DriveIOInputs inputs) {
+
+    inputs.gyroPositionRad = Math.toRadians(gyro.getAngleZ());
+    inputs.rightPositionRad = (rightEncoder.getDistance() / 1440) * 2 * Math.PI;
+    inputs.leftPositionRad = (leftEncoder.getDistance() / 1440) * 2 * Math.PI;
+    double leftVolts = leftPID.calculate(leftEncoder.getRate()) + leftFFVolts;
+    double rightVolts =
+        rightPID.calculate(rightEncoder.getRate()) + rightFFVolts;
+    setVoltage(leftVolts, rightVolts);
+
+
+  }
+
+  @Override
+  public void setVoltage(double leftVolts, double rightVolts) {
+
+    leftMotor.setVoltage(leftVolts);
+    rightMotor.setVoltage(rightVolts);
+
+  }
+
+  @Override
+  public void setVelocity(double leftVelocityRadPerSec,
+      double rightVelocityRadPerSec, double leftFFVolts, double rightFFVolts) {
+
+    closedLoop = true;
+    leftPID.setSetpoint(leftVelocityRadPerSec);
+    rightPID.setSetpoint(rightVelocityRadPerSec);
+    this.leftFFVolts = leftFFVolts;
+    this.rightFFVolts = rightFFVolts;
+
+  }
+
+  @Override
+  public void configurePID(double kp, double ki, double kd) {
+
+  }
+
+  @Override
+  public void resetPosition(double leftPositionRad, double rightPositionRad) {
+    leftEncoder.reset();
+    rightEncoder.reset();
+  }
+}
