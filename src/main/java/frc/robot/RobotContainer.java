712a1d967f98d3f8b6539f8a20cbb891d202c749
Jonah <47046556+jonahb55@users.noreply.github.com>
2022-02-12 16:32:53 -0500

    Fix "Do Nothing" auto routine

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -11,6 +11,7 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.Constants.Mode;
 import frc.robot.commands.AutoAim;
@@ -207,8 +208,8 @@ public class RobotContainer {
         new RunClimber(climber, () -> handheldOI.getClimbStick()));
 
     // Set up auto routines
-    autoRoutineMap.put("Do Nothing", new AutoRoutine(AutoPosition.ORIGIN,
-        new PrepareShooter(flywheels, hood, ShooterPreset.UPPER_FENDER)));
+    autoRoutineMap.put("Do Nothing",
+        new AutoRoutine(AutoPosition.ORIGIN, new InstantCommand()));
 
     autoRoutineMap.put("Five cargo (TD)",
         new AutoRoutine(AutoPosition.TARMAC_D, new FiveCargoAuto(drive, vision,
