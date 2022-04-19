6fd2306413751fea27c3ed01f4d12042d947cef9
Jonah <47046556+jonahb55@users.noreply.github.com>
2022-04-19 18:03:33 -0500

    Add tunable numbers to auto climb sequence

--- src/main/java/frc/robot/commands/climber/AutoClimb.java
+++ src/main/java/frc/robot/commands/climber/AutoClimb.java
@@ -6,25 +6,38 @@ package frc.robot.commands.climber;
 
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.subsystems.climber.Climber;
 import frc.robot.subsystems.drive.Drive;
 import frc.robot.subsystems.leds.Leds;
+import frc.robot.util.TunableNumber;
+import frc.robot.util.TunableWaitCommand;
 
 
 public class AutoClimb extends SequentialCommandGroup {
-  private static double downPullVolts = 6.0;
+  private static final double downPullVolts = 6.0;
+
+  private static final TunableNumber midToHighStartDelay =
+      new TunableNumber("AutoClimb/MidToHighStartDelay", 0.1);
+  private static final TunableNumber midToHighPullDelay =
+      new TunableNumber("AutoClimb/MidToHighPullDelay", 0.6);
+  private static final TunableNumber highToTraversalStartDelay =
+      new TunableNumber("AutoClimb/HighToTraversalStartDelay", 0.6);
+  private static final TunableNumber highToTraversalPullDelay =
+      new TunableNumber("AutoClimb/HighToTraversalPullDelay", 0.5);
 
   /** Creates a new AutoClimb. */
   public AutoClimb(Climber climber, Drive drive, Leds leds) {
     addCommands(new RunClimberToPosition(climber, climber.minPositionRad.get()),
-        new RunClimberToBottom(climber, downPullVolts), new WaitCommand(0.1),
+        new RunClimberToBottom(climber, downPullVolts),
+        new TunableWaitCommand(midToHighStartDelay),
         new RunClimberToPosition(climber, climber.maxPositionRad.get()),
-        new WaitCommand(0.6),
+        new TunableWaitCommand(midToHighPullDelay),
         new RunClimberToPosition(climber, climber.minPositionRad.get()),
-        new RunClimberToBottom(climber, downPullVolts), new WaitCommand(0.6),
+        new RunClimberToBottom(climber, downPullVolts),
+        new TunableWaitCommand(highToTraversalStartDelay),
         new RunClimberToPosition(climber, climber.maxPositionRad.get()),
-        new WaitCommand(0.5), new RunClimberToPosition(climber, 10.0),
+        new TunableWaitCommand(highToTraversalPullDelay),
+        new RunClimberToPosition(climber, 10.0),
         new InstantCommand(() -> leds.setClimbSuccess(true)));
   }
 }
