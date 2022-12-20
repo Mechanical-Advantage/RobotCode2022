b63561240e87b6a602a17b7faa50c652c2a95bc5
Jonah <47046556+jonahb55@users.noreply.github.com>
2022-12-20 01:17:32 -0500

    Update to BSD license

--- src/main/java/frc/robot/subsystems/drive/DriveIOTalonSRX.java
+++ src/main/java/frc/robot/subsystems/drive/DriveIOTalonSRX.java
@@ -1,7 +1,7 @@
 // Copyright (c) 2022 FRC 6328
 // http://github.com/Mechanical-Advantage
 //
-// Use of this source code is governed by an MIT-style
+// Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file at
 // the root directory of this project.
 
